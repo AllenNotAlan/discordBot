a742c241e576d74cabd315ad4bb482adf552f4cd
AllenNotAlan <allenloyola23@gmail.com>
2022-03-09 12:35:23 +0000

    removed commented web scraper func

--- main.py
+++ main.py
@@ -9,20 +9,6 @@ import gifsend
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))#gets the absolute directory of the environment (os)
 
-# @tasks.loop(seconds=60)
-# async def scrape():
-#     versionHeader = webscraper.sendMessage(URL)
-
-#     global newestVersion
-#     if newestVersion == None:
-#         newestVersion = versionHeader
-
-#     if versionHeader != newestVersion:
-#         newestVersion = versionHeader
-#         channel = client.get_channel(os.environ.get("CHANNEL_ID_1"))
-#         messageToSend = 'New version of ValheimPlus has been released {v}. See {url}.'.format(v=newestVersion, url=URL)
-#         await channel.send(messageToSend)
-
 def main():
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
